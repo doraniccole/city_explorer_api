9c9f7c88efa162fa8e7d81917aa12ecaea00dfd5
DORA BLAKEY DIANA <DORANICCOLE@YAHOO.COM>
2020-10-13 20:56:45 -0600

    lab 06 adjustments

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 'use strict'
-//pull in any envirionment variables (process.env) that live in a .env file
+//pull in any environment variables (process.env) that live in a .env file
 require('dotenv').config();
-//the above "dotenv" require is the same as 
-//const dotenv = require('dotenv')
-//dotenv.config();
-// ? const { response, request } = require('express');
 const express = require('express');
 //pull in express and assign the whole app to express
 const cors = require('cors');
@@ -12,17 +8,12 @@ const cors = require('cors');
 const app = express ();
 app.use(cors());
 
-//assign a port or location for accepting incoming traffic
-// developers often default their dev port to 3000 or 3001 for backgrounds
-// and often 8000 8080 for front ends
 const PORT = process.env.PORT || 3000;
-
 
 app.get('/', (request, response) => {
     response.send('my homepage');
-
 });
-//show how we produce app errors:
+
 app.get('/unauthorized', (request, response) => {
     throw new Error('not authorized to access this route');
 });
@@ -33,7 +24,7 @@ app.get('/test/route', (request, response) => {
     response.json({ location: 'seattle', temp: '47 deg'});
 });
 
-app.get('wester', (request, response) => {
+app.get('weather', (request, response) => {
     //go get the weather from somewhere
     //build and aggregate/approval ser of data to send to the user
     //send the weather data that apporve back to the client
@@ -49,7 +40,7 @@ function Location (city, geoData) {
     this.longitude = geoData [0];
 }
 
-functions handleLocation(request, response) {
+function handleLocation(request, response) {
 try{
 // try to resolve the following with no errors
 const geoData = require('./data/location.json');
@@ -60,14 +51,13 @@ response.json(locationData);
     //otherwise, if an error occurs
     // handle error here
     response.status(500).send('sorry, something broke.');
-}
-}
+}}
 
 app. get('*', (request, response) => {
     response.status(404).send('not found');
 });
 
-// configure our app t oaccept and listen for incoming traffic
+// configure our app to accept and listen for incoming traffic
 app.listen(PORT, () => {
     console.log(`server up: ${PORT}`);
 
