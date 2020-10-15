25eecce4b45fbc7525fb3f88884682331848d0f5
DORA BLAKEY DIANA <DORANICCOLE@YAHOO.COM>
2020-10-15 05:01:00 -0600

    troubleshooting deployment error

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(cors()); //creating new cors service-- app will use cors service
 
 const locations = {};
 
-app.get('/location', locationHandler);
-app.get('/restaurants', restaurantHandler);
+app.get('/location', locationHandler); //express (app) retrieves url, location function
+app.get('/restaurants', restaurantHandler); 
 app.get('/places', placesHandler);
 app.use('*', notFoundHandler);
 
@@ -31,32 +31,6 @@ app.get('/test/route', (request, response) => {
     //see an API as data that can be used to power other applications 
     response.json({ location: 'seattle', temp: '47 deg'});
 });
-
-app.get('weather', (request, response) => {
-    //go get the weather from somewhere
-    //build and aggregate/approval set of data to send to the user
-    //send the weather data that approve back to the client
-    //res.json({weatherDataFromAPIorDB})
-});
-function handleWeather(request, response) {
-    // declare a variable named weather data that has its contents as the entire json file 
-    let weatherData = require('./data/weather.json');
-    // declaring a new array called days
-    const days = [];
-    // for each day in weatherData, we will create a new Object
-    weatherData.data.forEach(day => days.push(new Day(day)));
-    response.send(days);
-}
-
-function Day(dayData) {
-    this.forecast = dayData.weather.description;
-    this.time = dayData.datetime;
-}
-//weather function Mark and Kim
-
-//http://locationhost:3000/location?city=slc&county=sl
-app.get('/location', handleLocation);
-
 function Location (city, geoData) {
     this.search_query = city;
     this.formatted_query = geoData[0].display_name;
@@ -82,33 +56,57 @@ app. get('*', (request, response) => {
     response.status(404).send('not found');
 });
 
-// configure our app to accept and listen for incoming traffic
+// configure app to accept and listen for incoming traffic
 app.listen(PORT, () => {
     console.log(`server up: ${PORT}`);
 
 });
 
-const superagent = require('superagent');
+//http://locationhost:3000/location?city=slc&county=sl
+app.get('/location', handleLocation);
+//location functions from lecture
 
-// callback
-superagent
-    .post('https://www.weatherbit.io/account/dashboard')
-    .send({ weather: '', storms: '' }) // sends a JSON post body
-    .set('https://www.weatherbit.io/account/dashboard', '79b23f2d2f38464fb88f1a20231b5c78')
-    .set('accept', 'json')
-    .end((err, res) => {
-        // Calling the end function will send the request
-    });
+// app.get('weather', (request, response) => {
+//     //go get the weather from somewhere
+//     //build and aggregate/approval set of data to send to the user
+//     //send the weather data that approve back to the client
+//     //res.json({weatherDataFromAPIorDB})
+// });
+// function handleWeather(request, response) {
+//     // declare a variable named weather data held in local json file 
+//     let weatherData = require('./data/weather.json');
+//     // declaring a new array called days
+//     const days = [];
+//     // for each day in weatherData, we will create a new Object
+//     weatherData.data.forEach(day => days.push(new Day(day)));
+//     response.send(days);
+// }
 
-// promise with then/catch
-superagent.post('').then(console.log).catch(console.error);
+// function Day(dayData) {
+//     this.forecast = dayData.weather.description;
+//     this.time = dayData.datetime;
+// }
+// //weather function Mark and Kim
 
-// promise with async/await
-(async () => {
-    try {
-        const res = await superagent.post('');
-        console.log(res);
-    } catch (err) {
-        console.error(err);
-    }
-})();
+// // callback
+// superagent
+//     .post('https://www.weatherbit.io/account/dashboard')
+//     .send({ weather: '', storms: '' }) // sends a JSON post body
+//     .set('https://www.weatherbit.io/account/dashboard', '79b23f2d2f38464fb88f1a20231b5c78')
+//     .set('accept', 'json')
+//     .end((err, res) => {
+//         // Calling the end function will send the request
+//     });
+
+// // promise with then/catch
+// superagent.post('').then(console.log).catch(console.error);
+
+// // promise with async/await
+// (async () => {
+//     try {
+//         const res = await superagent.post('');
+//         console.log(res);
+//     } catch (err) {
+//         console.error(err);
+//     }
+// })(); //
